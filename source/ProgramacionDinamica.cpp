591fe41ede6d92e19547cef71d040ac2fe9eb404
maialonso <maia.alonso2005@gmail.com>
2026-04-11 19:45:24 -0300

    Comentarios y emprolijamos

--- source/ProgramacionDinamica.cpp
+++ source/ProgramacionDinamica.cpp
@@ -75,43 +75,32 @@ std::vector<int> reconstruccion(const std::vector<std::vector<double>>& energia,
     while(i >= 0){
         int posAnterior= memo[i][j].first;  // valor de la pos del elemento anterior
         respuesta.push_back(posAnterior);
-        j = posAnterior;//actualizo la columna en donde estoy
+        j = posAnterior;                    // actualiza la columna en donde estoy
         i--;
     }
 
-    //llamo a la funcion que invierte la solucion, asi se devuelve el vector en el orden esperado
-    std::vector<int> res= invertirPairs(respuesta);
+    std::vector<int> res= invertirPairs(respuesta);         // llamado a la función que invierte la solución 
     return res;
-
 }
 
+
+// función principal
 std::vector<int> encontrarSeamPDNueva(const std::vector<std::vector<double>>& energia) {
     if (energia.empty() || energia[0].empty()) {
         return {};
     }
     //creo el memo
-    int n= energia.size();
-    int m= energia[0].size();
-    //memo= vector de vectores pair<int: columna "de donde viene", double: energia acumulada>
-    std::vector<std::vector<std::pair<int, double>>> memo(
-    n,
-    std::vector<std::pair<int, double>>(m, {-1, std::numeric_limits<double>::infinity()}));
+    int n= energia.size();          // filas
+    int m= energia[0].size();       // columnas
+    std::vector<std::vector<std::pair<int, double>>> memo(n, std::vector<std::pair<int, double>>(m, {-1, std::numeric_limits<double>::infinity()}));    // memo = vector de vectores pair<int: columna "de dónde viene la mejor costura", double: energía acumulada>
 
     //se hace un llamado de la auxiliar por columna
     for(int j = 0; j < m; j++){
         encontrarSeamPDRec(energia, n-1, j, n, m, memo);
     }   
     
-    /*ahora busco el minimo-> luego del primer ciclo, todos los elementos de la ultima fila 
-      ya estan resueltos con los minimos caminos llenos de energia*/
-
-    //primer elemento de la ultima fila del memo(primer valor posible)
-    std::pair<int, double> min= memo[n-1][0];
-    
-    //posicion con la que voy a hacer la recursion para reconstruir el camino
-    int pos=0;
-
-    //empiezo a comparar desde el primero
+    std::pair<int, double> min = memo[n-1][0];      // toma al elemento de la primer columna de la última fila como min para empezar
+    int pos=0;                                      // guarda la posición con la que luego querrá reconstruir el camino
     for(int i=1; i<m; i++){
         if(min.second>memo[n-1][i].second){
             min = memo[n-1][i];
@@ -119,7 +108,6 @@ std::vector<int> encontrarSeamPDNueva(const std::vector<std::vector<double>>& en
         }
     }
 
-    //hago para la reconstruccion
-    std::vector<int> posColumna= reconstruccion(energia, memo, pos);
+    std::vector<int> posColumna= reconstruccion(energia, memo, pos);        // reconstruye camino de la costura
     return posColumna;
 }
